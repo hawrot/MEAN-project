f33cd4bfd5bfd4ded87efa3b98871d066125f45a
hawrot <mateusz@sent.com>
2018-09-19 13:59:17 +0100

    implementation of nodejs/express and mongoDB

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,31 +1,39 @@
-import {Post} from './post.model';
-import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
 
-@Injectable({providedIn: 'root'})
-export class PostsService{
- private posts: Post[] = [];
- private postsUpdated = new Subject<Post[]>();
+import { Post } from "./post.model";
 
- constructor(private http: HttpClient){}
+@Injectable({ providedIn: "root" })
+export class PostsService {
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
- getPosts(){
-   this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts').subscribe((postData) => {
-   this.posts = postData.posts;
-  this.postsUpdated.next([...this.posts]);
-   });
+  constructor(private http: HttpClient) {}
 
- }
- getPostUpdateListener(){
-   return this.postsUpdated.asObservable();
- }
+  getPosts() {
+    this.http
+      .get<{ message: string; posts: Post[] }>(
+        "http://localhost:3000/api/posts"
+      )
+      .subscribe(postData => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
 
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
 
- addPost(title: string, content: string)
- {
-   const post: Post = {id: null, title: title, content: content};
-   this.posts.push(post);
-   this.postsUpdated.next([...this.posts]);
- }
+  addPost(title: string, content: string) {
+    const post: Post = { id: null, title: title, content: content };
+    this.http
+      .post<{ message: string }>("http://localhost:3000/api/posts", post)
+      .subscribe(responseData => {
+        console.log(responseData.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
 }
