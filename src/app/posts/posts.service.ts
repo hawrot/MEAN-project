9522dafa5ac6e5abee3b1051442f9c8b1279f33f
hawrot <mateusz@sent.com>
2018-09-20 14:47:23 +0100

    implementation of adding and deleting posts FRONT and BACK end

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Subject } from "rxjs";
-import {map} from "rxjs/operators";
+import { map } from 'rxjs/operators';
 
 import { Post } from "./post.model";
 
@@ -25,8 +25,7 @@ export class PostsService {
             id: post._id
           };
         });
-
-    }))
+      }))
       .subscribe(transformedPosts => {
         this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
@@ -40,10 +39,20 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string }>("http://localhost:3000/api/posts", post)
+      .post<{ message: string, postId: string }>("http://localhost:3000/api/posts", post)
       .subscribe(responseData => {
-        console.log(responseData.message);
+        const id = responseData.postId;
+        post.id = id;
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  deletePost(postId: string) {
+    this.http.delete("http://localhost:3000/api/posts/" + postId)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
